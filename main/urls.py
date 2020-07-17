7f0162e8acc40530ebdff65374309855cd385c1d
Madhur215 <vashistha.madhur2@gmail.com>
2020-07-18 02:06:18 +0530

    Complete index page

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/home", views.Home, name="home"),
-    path("", views.index, name="index"),
+    path("home/", views.Home, name="home"),
+    # path("", views.index, name="index"),
     # path("signup/", views.SignUp, name="SignUp"),
     # path("home/"),
     # path("register/"),
--- main/views.py
+++ main/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 
 
-def Home(response):
+def Home(request):
 
-    # if User.is_authenticated:
-    return render(response, "main/Home.html", {})
-    # else:
-    #     return render(response, "main/index.html")
+    if not request.user.is_authenticated:
+        print("Not logged In")
+        return render(request, "main/index.html", {})
+    else:
+        return render(request, "main/Home.html", {})
 
 
-def index(response):
-    return render(response, "main/index.html", {})
-    # return HttpResponse("<h1>Hello</h1>")
+# def index(response):
+#     return render(response, "main/index.html", {})
+#     # return HttpResponse("<h1>Hello</h1>")
